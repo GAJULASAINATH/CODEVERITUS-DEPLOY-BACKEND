2f6d9994d4ff6b1b9a1c3eb9c5bf8ca34c434609
Gajula Sainath <gajulasainath25@gmail.com>
2025-10-06 16:51:07 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+//access credentials
+
+//All packages
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+
+//routes
+const { connectDB } = require("./dbConnections/db");
+const usersRoute = require("./routes/usersRoutes");
+const adminsRoute = require("./routes/adminsRoutes");
+const jwtAuthenticator = require("./middleware/jwtAuthenticator");
+
+//MAIN APP
+const app = express();
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+//PORT AVAILABILITY
+const PORT = process.env.PORT || 5000;
+
+const serverAndDatabaseConnection = async () => {
+  try {
+    await connectDB();
+
+    //UNPROTEDTED ROUTES
+    app.use("/api/users", usersRoute);
+    app.use("/api/admins", adminsRoute);
+
+    //PROTECTED ROUTES
+    app.use("/api/admins/fetch", jwtAuthenticator, adminsRoute);
+
+    app.listen(PORT, "0.0.0.0", () => { 
+      console.log(`✅ Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Database Error: ${error.message}`);
+    process.exit(1);
+  } finally {
+    console.log("Server initialization attempted.");
+  }
+};
+
+//VISIT CODE
+app.get("/",(req,res)=>{
+  res.send("CODEVERITUS-BACKEND");
+})
+serverAndDatabaseConnection();
