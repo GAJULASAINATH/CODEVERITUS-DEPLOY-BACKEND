9bc0b240ef6037aa3601a3af73f8996d6284f031
Gajula Sainath <gajulasainath25@gmail.com>
2025-10-07 20:42:43 +0530

    http to httpS converted 6th time

--- app.js
+++ app.js
@@ -15,40 +15,49 @@ const jwtAuthenticator = require("./middleware/jwtAuthenticator");
 //MAIN APP
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
+
+// Trust proxy (Nginx)
+app.set('trust proxy', true);
+
+// CORS configuration to enforce HTTPS
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || origin.startsWith('https://codeveritus.tech')) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
-  })
-);
+}));
 
 //PORT AVAILABILITY
 const PORT = process.env.PORT || 4000;
 
 const serverAndDatabaseConnection = async () => {
-  try {
-    await connectDB();
+    try {
+        await connectDB();
 
-    //UNPROTEDTED ROUTES
-    app.use("/api/users", usersRoute);
-    app.use("/api/admins", adminsRoute);
+        //UNPROTEDTED ROUTES
+        app.use("/api/users", usersRoute);
+        app.use("/api/admins", adminsRoute);
 
-    //PROTECTED ROUTES
-    app.use("/api/admins/fetch", jwtAuthenticator, adminsRoute);
+        //PROTECTED ROUTES
+        app.use("/api/admins/fetch", jwtAuthenticator, adminsRoute);
 
-    app.listen(PORT, "0.0.0.0", () => { 
-      console.log(`✅ Server is running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.log(`Database Error: ${error.message}`);
-    process.exit(1);
-  } finally {
-    console.log("Server initialization attempted.");
-  }
+        app.listen(PORT, "0.0.0.0", () => { 
+            console.log(`✅ Server is running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(`Database Error: ${error.message}`);
+        process.exit(1);
+    } finally {
+        console.log("Server initialization attempted.");
+    }
 };
 
 //VISIT CODE
-app.get("/",(req,res)=>{
-  res.send("CODEVERITUS-BACKEND");
-})
+app.get("/", (req, res) => {
+    res.send("CODEVERITUS-BACKEND");
+});
 serverAndDatabaseConnection();
